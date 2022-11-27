80c3c981115588cb53b0708abd199d3d91f68891
admin <nbisametova@mail.ru>
2022-11-27 14:42:45 +0600

    3rd try

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -5,13 +5,13 @@ public class Pyramid {
         for (int i = 1; i <= cathetusLength; i++) {
             for (int j = 1; j <=cathetusLength; j++) {
                 if(cathetusLength-j<=i-1){
-                    System.out.print(cathetusLength-j+1+"");
+                    System.out.print(""+ (cathetusLength-j+1));
                 }else System.out.print(" ");
             }
 
             for (int k = cathetusLength-1;k>=0;k--){
                 if(cathetusLength-k<=i-1){
-                    System.out.print(cathetusLength-k+1+"");
+                    System.out.print(""+ (cathetusLength-k+1));
                 }else System.out.print(" ");
             }
             System.out.println();
