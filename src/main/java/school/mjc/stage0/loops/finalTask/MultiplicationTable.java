3e8fbcd5cb2e8f65e35947190d19d2d1df9e7535
admin <nbisametova@mail.ru>
2022-11-27 14:31:41 +0600

    2nd try

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -4,7 +4,7 @@ public class MultiplicationTable {
     public void printTable(int numberTableToPrint){
         for (int i = 1; i<=10;i++){
             int res = i*numberTableToPrint;
-            System.out.printf("%d x %d = %d \n",i, numberTableToPrint, res);
+            System.out.printf("%d x %d = %d\n",i, numberTableToPrint, res);
         }
     }
 }
